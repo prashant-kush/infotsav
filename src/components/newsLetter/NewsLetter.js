bed6852e0c51fdcdd8514bd01604cafa296ca15f
Prashant <prashantk.tps@gmail.com>
2019-03-21 18:20:13 +0530

    correcting regex

--- src/components/newsLetter/NewsLetter.js
+++ src/components/newsLetter/NewsLetter.js
@@ -1,7 +1,7 @@
 import React from "react";
 import email from "../../email2.png";
 import "./newsLetter.css";
-import Loader from "./../../components/loader/Loader.js";
+import Loader from "./../../components/loader/Loader.js"
 class NewsLetter extends React.Component
 {
 	constructor()
@@ -14,10 +14,29 @@ class NewsLetter extends React.Component
 						loading:"hidden"
 					}
 	}
-	onChange=()=>
+	onSubmit=()=>
 	{
-		this.setState({emailInput:document.getElementById("input").value,run:1});
-		if(this.state.run==!0)
+		this.setState({loading:"visible"});
+		return fetch("https://salty-fjord-26855.herokuapp.com/email",{
+			method:"post",
+			headers:{"Content-Type":"application/json"},
+			body:JSON.stringify({email:this.state.emailInput})
+		}).then((response)=>{
+			const res=response.json();
+			return res;
+		}).then(data=>
+		{
+			this.setState({loading:"hidden"});
+			alert(data);
+		}).catch(err=>{
+			this.setState({loading:"hidden"});
+			alert(err);});
+	}
+
+
+		render()
+		{
+			if(this.state.run==!0)
 			{
 			
 			const svg=document.getElementById("svg");
@@ -52,47 +71,26 @@ class NewsLetter extends React.Component
 					elm.parentNode.replaceChild(newone, elm);
 				  }
 				}
-	}
-	onSubmit=()=>
-	{
-		this.setState({loading:"visible"});
-		return fetch("https://salty-fjord-26855.herokuapp.com/email",{
-			method:"post",
-			headers:{"Content-Type":"application/json"},
-			body:JSON.stringify({email:this.state.emailInput})
-		}).then((response)=>{
-			const res=response.json();
-			return res;
-		}).then(data=>
-		{
-			this.setState({loading:"hidden"});
-			alert(data);
-		}).catch(err=>{
-			this.setState({loading:"hidden"});
-			alert(err);});
-	}
-
-
-		render()
-		{
 
 		return(
-				<div>
-				<Loader visibility={this.state.loading}/>
+			<div>
+				<Loader visibility={this.state.loading} />
 				<div className="news">
 					<div className="message">
 						<p className="newsletter">We'll be here soon with a newer version of INFOTSAV, subscribe to be notified.</p>
 					</div>
 					<div className="input_field">
 						<img src={email} alt="email_svg" className="email_png"/>
-						<input type="text" placeholder="Enter your email here" className="input_box" id="input" onChange={this.onChange} />
+						<input type="text" placeholder="Enter your email here" className="input_box" id="input" onChange={()=>{
+							this.setState({emailInput:document.getElementById("input").value,run:1});
+						}} />
 						<svg className="checkmark1" id="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"
 							onClick={this.onSubmit}><circle className="checkmark__circle1" id="circle" cx="26" cy="26" r="25" fill="none"
 							/><path className="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
 						</svg>
 					</div>
 				</div>
-				</div>
+			</div>
 			);
 		}
 }
