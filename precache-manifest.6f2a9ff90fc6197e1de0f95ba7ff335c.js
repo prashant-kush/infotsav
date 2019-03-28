dae306952f080086e60772e8c46779678c025197
Prashant <prashantk.tps@gmail.com>
2019-03-28 23:01:47 +0530

    Updates

---
+++ precache-manifest.6f2a9ff90fc6197e1de0f95ba7ff335c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6178640e15edc693031f",
+    "url": "/infotsav-landing-page-2019/static/js/runtime~main.6178640e.js"
+  },
+  {
+    "revision": "b466f04529f41efab2b4",
+    "url": "/infotsav-landing-page-2019/static/js/main.b466f045.chunk.js"
+  },
+  {
+    "revision": "ebdfba6fa05a7527d12a",
+    "url": "/infotsav-landing-page-2019/static/js/2.ebdfba6f.chunk.js"
+  },
+  {
+    "revision": "b466f04529f41efab2b4",
+    "url": "/infotsav-landing-page-2019/static/css/main.02c1cd25.chunk.css"
+  },
+  {
+    "revision": "eecceff85f80b80b5134ae79ad51b3c2",
+    "url": "/infotsav-landing-page-2019/index.html"
+  }
+];
